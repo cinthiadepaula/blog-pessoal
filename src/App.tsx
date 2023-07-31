38945ab75cd0a24fd52385cd97e945f63627d60c
cinthiadepaula <cinthiade.paula@outlook.com>
2023-07-31 18:27:22 -0300

    cardTema

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 import Cadastro from './paginas/cadastro/Cadastro';
-
+import ListarTemas from "./components/temas/listaTemas/ListaTemas"
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
 function App() {
 
@@ -19,12 +21,16 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <div className='min-h-[80vh]'>
-        <Routes>
-          <Route path="/" element={<Cadastro />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
+      <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListarTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
+            </Routes>
       </div>
       <Footer />
     </BrowserRouter>
